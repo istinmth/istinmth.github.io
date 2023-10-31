f026b6e9b90a2348f4749c43add68af3ae71bb37
istinmth <istinmth@icloud.com>
2023-10-31 12:23:39 +0100

    Update for mobile breakpoints

--- js/cinnamon.js
+++ js/cinnamon.js
@@ -1,2 +1,2 @@
 let cinnamonRollCount = 0;
-setInterval(() => document.getElementById("cinnamonRollCount").textContent = ++cinnamonRollCount, 5000);
+setInterval(() => document.getElementById("cinnamonRollCount").textContent = ++cinnamonRollCount, 10000);
