ecb04de9df50267738cb546fb7f5a7eaf5628a7d
istinmth <istinmth@icloud.com>
2023-10-31 12:34:34 +0100

    Updated margins and float-in behaviour

--- js/scroll.js
+++ js/scroll.js
@@ -2,15 +2,22 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         // If the element is intersecting with the viewport, add the 'show' class
-        // If it's not intersecting, remove the 'show' class
-        entry.target.classList[entry.isIntersecting ? 'add' : 'remove']('show');
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            // Stop observing the element so the animation only happens once
+            observer.unobserve(entry.target);
+        }
     });
 });
+
 // Select all elements with the class 'hidden'
 // Start observing each of these elements with the observer
 document.querySelectorAll('.hidden').forEach(ent => observer.observe(ent));
+
 var element = document.querySelector('.multiplikativ');
 // This event is fired when a CSS Animation has completed
 element.addEventListener('animationend', function() {
     this.classList.add('after-animation');
+    // Remove the event listener so the 'after-animation' class is only added once
+    this.removeEventListener('animationend', arguments.callee);
 });
