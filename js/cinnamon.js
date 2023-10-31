be1cf8b4a88b54227c8babd003d9f7ff08fea6a6
istinmth <istinmth@icloud.com>
2023-10-31 23:08:59 +0100

    lots of structural updates

--- js/cinnamon.js
+++ js/cinnamon.js
@@ -1,2 +1,6 @@
-let cinnamonRollCount = 0;
-setInterval(() => document.getElementById("cinnamonRollCount").textContent = ++cinnamonRollCount, 10000);
+let cinnamonRollCount = parseInt(localStorage.getItem('cinnamonRollCount')) || 0;
+document.getElementById("cinnamonRollCount").textContent = cinnamonRollCount;
+setInterval(() => {
+    document.getElementById("cinnamonRollCount").textContent = ++cinnamonRollCount;
+    localStorage.setItem('cinnamonRollCount', cinnamonRollCount);
+}, 20000);
