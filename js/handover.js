e71141a674c8c93e33e8d740cc5f20b3cc324774
istinmth <istinmth@icloud.com>
2023-11-29 14:37:47 +0100

    hát ez nem működött hagyjuk is

--- js/handover.js
+++ js/handover.js
@@ -4,27 +4,22 @@ $("#foxForm").on('submit', function(fox) {
         return false;
     } else {
         // Form is valid, get the input values
-        var nev = btoa($("#name-input").val());
-        var email = btoa($("#email-input").val());
-        var bunda = btoa($("#bunda-input").val());
-        var nyuszi = btoa($("#nyuszi").is(":checked").toString());
-        var eper = btoa($("#eper").is(":checked").toString());
-        var nutella = btoa($("#nutella").is(":checked").toString());
-        var kukac = btoa($("#kukac").is(":checked").toString());
-        var igenNem = btoa($("input[name='igen-nem']:checked").val());
-        var hossz = btoa($("#range-input").val());
-        var szin = btoa($("#color-input").val());
+        const data = {
+            nev: btoa($("#name-input").val()),
+            email: btoa($("#email-input").val()),
+            bunda: btoa($("#bunda-input").val()),
+            nyuszi: btoa($("#nyuszi").is(":checked").toString()),
+            eper: btoa($("#eper").is(":checked").toString()),
+            nutella: btoa($("#nutella").is(":checked").toString()),
+            kukac: btoa($("#kukac").is(":checked").toString()),
+            igenNem: btoa($("input[name='igen-nem']:checked").val()),
+            hossz: btoa($("#range-input").val()),
+            szin: btoa($("#color-input").val()),
+        };
 
         // Redirect the user and pass the form values as URL parameters
-        window.location.href = "submitted.html?nev=" + nev +
-            "&email=" + email +
-            "&bunda=" + bunda +
-            "&nyuszi=" + nyuszi +
-            "&eper=" + eper +
-            "&nutella=" + nutella +
-            "&kukac=" + kukac +
-            "&igenNem=" + igenNem +
-            "&hossz=" + hossz +
-            "&szin=" + szin;
+        const query = Object.entries(data)
+            .map(([key, value]) => `${key}=${value}`).join('&');
+        window.location.href = "sumbitted.html?" + query;
     }
 });
--- js/viewport-corrector.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', setRealViewHeight);
-window.addEventListener('resize', setRealViewHeight);
-
-function setRealViewHeight() {
-    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-}
